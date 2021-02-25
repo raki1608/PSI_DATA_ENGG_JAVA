78e1821559a7dee34520743e9b1e9b2d766004cf
megbaliga <79622748+megbaliga@users.noreply.github.com>
2021-02-25 11:54:02 +0530

    megha changes

---
+++ megha1/vars.java
@@ -0,0 +1,7 @@
+class A {
+	int data = 50;
+	static int data1 = 30;
+	public static void main(String [] args) {
+		int n = 70;
+	}
+}
---
+++ megha1/widee.java
@@ -0,0 +1,10 @@
+class convv {
+	public static void main(String [] args) {
+		
+		float f = 10.99f;
+		int a = (int)f;
+		System.out.println(a);
+		System.out.println(f);
+	}
+}
+		
